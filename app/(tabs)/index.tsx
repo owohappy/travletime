58031f66f8854de1569ec68c343a2d14e2d9f470
lucas <lucas.lucas1304@gmail.com>
2025-05-11 18:12:56 +0200

    wg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,97 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button } from '@react-navigation/elements'
+import { Stack } from 'expo-router'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const app = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={style.container}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <View style={[style.boxForground, { backgroundColor:  '#ffffff' }]}>
+        <Text 
+        style={style.welcomeText}>
+          Welcome back 👋
+        </Text>
+        
+        <Text 
+        style={style.underText}>
+          Login with you traveltime account 
+        </Text>
+
+        <TextInput 
+        style={style.email}
+        value='E-Mail'
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+
+        <TextInput 
+        style={style.password}
+        value='Password'
+        />
+
+        <Button style={style.loginButton}>
+        Login
+        </Button>
+    
+        </View>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
+export default app
+
+const style = StyleSheet.create({
+  boxForground:{
+    color: 'white',
+    width: '100%',
+    height: screen.availHeight / 1.3,
+    zIndex: 0,
     bottom: 0,
-    left: 0,
     position: 'absolute',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
   },
-});
+  container: {
+    flex: 1, 
+    flexDirection: 'column',
+    color: 'white',
+    backgroundColor: '#121212',
+    justifyContent: 'flex-start'
+  },
+  welcomeText:{
+    color: 'black',
+    alignSelf: 'center',
+    paddingTop: screen.availHeight / 7,
+    fontSize: 24,
+    fontWeight: '900'
+  },
+  underText:{
+    color: 'black',
+    alignSelf: 'center',
+    fontSize: 12,
+    fontWeight: '200'
+  },
+  email: {
+    height: 40,
+    margin: 48,
+    marginTop: screen.availHeight / 12.7,
+    borderWidth: 1, 
+    borderColor: 'dimgray',
+    color: 'black',
+    padding: 12,
+    borderRadius: 12
+  },
+  password: {
+    height: 40,
+    margin: 48,
+    marginTop: -32,
+    borderWidth: 1, 
+    borderColor: 'dimgray',
+    color: 'black',
+    padding: 12,
+    borderRadius: 12
+  },
+  loginButton: {
+    alignSelf: 'center'
+  }
+})

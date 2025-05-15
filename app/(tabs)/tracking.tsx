4338df8da259fa3504064d8266e9991f0922cc4d
owohappy <102672314+owohappy@users.noreply.github.com>
2025-05-15 08:33:06 +0000

    Add trackingBackground component file

--- app/(tabs)/tracking.tsx
+++ app/(tabs)/tracking.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 export default function tracking() {
   return (
     <View>
-      <Text>tracking</Text>
+
+
     </View>
   )
 }

121d4ea7b49a601799f6b4e0732d268307008ec2
owohappy <102672314+owohappy@users.noreply.github.com>
2025-05-16 13:26:31 +0000

    Add initial implementation of RewardPage component

---
+++ app/(tabs)/rewardspage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function rewardspage() {
+  return (
+    <View>
+      <Text>rewardspage</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
